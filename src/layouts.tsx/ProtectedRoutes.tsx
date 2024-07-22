0d1e77bcc29c75dd245941335ba98b74306793dc
longani974 <sylv1.riviere@gmail.com>
2024-07-22 19:32:39 +0400

    remove logout button

--- src/layouts.tsx/ProtectedRoutes.tsx
+++ src/layouts.tsx/ProtectedRoutes.tsx
@@ -1,23 +1,9 @@
-// import { useUser } from '@/context/AuthContext';
-import { Button } from '@/components/ui/button';
-import { useAuth } from '@/context/AuthContext';
-// import { account } from '@/lib/appWrite/config';
-// import { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const ProtectedRoutes = () => {
-    const navigate = useNavigate();
-    const { logout } = useAuth();
-
-    const handleLogout = async () => {
-        await logout();
-        navigate('/sign-in');
-    };
-
     return (
         <>
             <Outlet />
-            <Button onClick={handleLogout}>Logout</Button>
         </>
     );
 };
